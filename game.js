bc538feaf84750608594dab18af70c1716d467e3
Jen Williams <101885864+JenW79@users.noreply.github.com>
2023-03-18 18:33:08 -0500

    Update game.js

--- game.js
+++ game.js
@@ -2,9 +2,14 @@ let player1Score = 0;
 let player2Score = 0;
 const totalClicksNeeded = 1000;
 
+let isPlayer1Turn; // declare the variable outside the event listener
+
 const clickButton = document.getElementById('clickButton');
 clickButton.addEventListener('click', function() {
   if (player1Score < totalClicksNeeded && player2Score < totalClicksNeeded) {
+    if (isPlayer1Turn === undefined) { // initialize the variable with a boolean value
+      isPlayer1Turn = Math.random() < 0.5;
+    }
     if (isPlayer1Turn) {
       player1Score++;
       document.getElementById('player1Score').textContent = player1Score;
@@ -18,10 +23,5 @@ clickButton.addEventListener('click', function() {
     } else if (player2Score === totalClicksNeeded) {
       alert('Player 2 Wins!');
     }
-  }
-
-  let isPlayer1Turn = Math.random() < 0.5;
-  if (!isPlayer1Turn) {
-    document.body.classList.add('player2Turn');
   }
 });
