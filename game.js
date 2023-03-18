86a92f529e4f4ff1943c3d9943260373d8c87c11
Jen Williams <101885864+JenW79@users.noreply.github.com>
2023-03-18 18:53:19 -0500

    Add files via upload
    Added additional values to the players element

--- game.js
+++ game.js
@@ -1,35 +1,54 @@
 let player1Score = 0;
 let player2Score = 0;
-const totalClicksNeeded = 1000;
+let totalClicksNeeded = 100;
+let currentPlayer;
 
-let isPlayer1Turn = true;
+const startButton = document.getElementById('startButton');
+startButton.addEventListener('click', function() {
+  const player1Name = document.getElementById('player1Name').value;
+  const player2Name = document.getElementById('player2Name').value;
+  initializeGame(player1Name, player2Name);
+  setCurrentPlayer(player1Name);
+});
+
+function initializeGame(player1Name, player2Name) {
+  player1Score = 0;
+  player2Score = 0;
+  totalClicksNeeded = 100;
+  currentPlayer = player1Name;
+  document.getElementById('player1NameDisplay').textContent = player1Name;
+  document.getElementById('player2NameDisplay').textContent = player2Name;
+}
+
+function setCurrentPlayer(playerName) {
+  currentPlayer = playerName;
+  document.getElementById('currentPlayer').textContent = playerName;
+}
 
 const clickButton = document.getElementById('clickButton');
 clickButton.addEventListener('click', function() {
   if (player1Score < totalClicksNeeded && player2Score < totalClicksNeeded) {
-    if (isPlayer1Turn) {
+    if (currentPlayer === player1Name) {
       player1Score++;
       document.getElementById('player1Score').textContent = player1Score;
+      setCurrentPlayer(player2Name);
     } else {
       player2Score++;
       document.getElementById('player2Score').textContent = player2Score;
+      setCurrentPlayer(player1Name);
     }
-    isPlayer1Turn = !isPlayer1Turn;
-    const currentPlayerName = isPlayer1Turn ? player1Name : player2Name;
-    document.getElementById('currentPlayer').textContent = currentPlayerName;
     if (player1Score === totalClicksNeeded) {
-      alert('Player 1 Wins!');
+      alert(`${player1Name} Wins!`);
     } else if (player2Score === totalClicksNeeded) {
-      alert('Player 2 Wins!');
+      alert(`${player2Name} Wins!`);
     }
   }
 });
 
 const player1Name = prompt("Player 1, please enter your name:");
 if (player1Name) {
-  document.getElementById('player1Name').textContent = player1Name;
+  document.getElementById('player1Name').value = player1Name;
 }
 const player2Name = prompt("Player 2, please enter your name:");
 if (player2Name) {
-  document.getElementById('player2Name').textContent = player2Name;
-}
+  document.getElementById('
